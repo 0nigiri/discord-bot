f927e60e18d1f93bf6e30981cae922be5f6fde07
0nigiri <thiagonixi@gmail.com>
2023-02-23 10:01:47 -0300

    bug fix

--- src/commands/misc/calcular.js
+++ src/commands/misc/calcular.js
@@ -21,7 +21,7 @@ module.exports = {
             maxValue: 6000,
         }
     ],
-    callback: (client, interaction) => {
+    callback: async (client, interaction) => {
         const mmrAtual = interaction.options.get('mmr-atual').value;
         const mmrDesejado = interaction.options.get('mmr-desejado').value;
         const numberGames = Math.ceil((mmrDesejado - mmrAtual) / 30)
@@ -32,92 +32,94 @@ module.exports = {
         // console.log(numberGames);
 
         if (mmrAtual > mmrDesejado) {
-            interaction.reply({
+            await interaction.reply({
                 content: 'MMR atual maior que o MMR desejado.',
                 ephemeral: true,
-            });
-        }
 
-        if ((mmrDesejado - mmrAtual) < 300) {
-            interaction.reply({
+            });
+
+        } else if ((mmrDesejado - mmrAtual) < 300) {
+
+
+            await interaction.reply({
                 content: 'MMR desejado abaixo do valor minimo, o boost tem que ter no minimo 300 de mmr de diferença.',
                 ephemeral: true,
             });
+        } else {
+
+            for (let i = 0; i < numberGames; i++) {
+                mmr += 30;
+
+
+
+                if (mmr < 800) {
+                    valorSolo += 5;
+                    valorParty += 8;
+                    continue;
+                }
+
+                if (801 < mmr && mmr < 1500) {
+                    valorSolo += 5.25;
+                    valorParty += 9;
+                    continue;
+                }
+
+                if (1501 < mmr && mmr < 2200) {
+                    valorSolo += 5.5;
+                    valorParty += 9;
+                    continue;
+                }
+                if (2201 < mmr && mmr < 2750) {
+                    valorSolo += 6;
+                    valorParty += 10;
+                    continue;
+                }
+                if (2751 < mmr && mmr < 3300) {
+                    valorSolo += 6.85;
+                    valorParty += 11;
+                    continue;
+                }
+                if (3301 < mmr && mmr < 3800) {
+                    valorSolo += 7.5;
+                    valorParty += 13;
+                    continue;
+                }
+                if (3801 < mmr && mmr < 4100) {
+                    valorSolo += 9;
+                    valorParty += 16;
+                    continue;
+                }
+                if (4101 < mmr && mmr < 4500) {
+                    valorSolo += 10.8;
+                    valorParty += 19;
+                    continue;
+                }
+                if (4501 < mmr && mmr < 5000) {
+                    valorSolo += 13.5;
+                    valorParty += 21;
+                    continue;
+                }
+                if (5001 < mmr && mmr < 5400) {
+                    valorSolo += 19;
+                    valorParty += 22;
+                    continue;
+                }
+                if (5401 < mmr && mmr < 6000) {
+                    valorSolo += 32;
+                    valorParty += 52;
+                    continue;
+                }
+            }
+            // console.log(valorSolo);
+            // console.log(valorParty);
+
+            valorSolo = Math.ceil(valorSolo);
+            valorParty = Math.ceil(valorParty);
+
+            await interaction.reply({
+                content: `O valor do boost jogando em sua conta é R$ ${valorSolo} e o valor jogando em party é ${valorParty}`,
+                ephemeral: true,
+            });
         }
-
-        for (let i = 0; i < numberGames; i++) {
-            mmr += 30;
-
-
-
-            if (mmr < 800) {
-                valorSolo += 5;
-                valorParty += 8;
-                continue;
-            }
-
-            if (801 < mmr && mmr < 1500) {
-                valorSolo += 5.25;
-                valorParty += 9;
-                continue;
-            }
-
-            if (1501 < mmr && mmr < 2200) {
-                valorSolo += 5.5;
-                valorParty += 9;
-                continue;
-            }
-            if (2201 < mmr && mmr < 2750) {
-                valorSolo += 6;
-                valorParty += 10;
-                continue;
-            }
-            if (2751 < mmr && mmr < 3300) {
-                valorSolo += 6.85;
-                valorParty += 11;
-                continue;
-            }
-            if (3301 < mmr && mmr < 3800) {
-                valorSolo += 7.5;
-                valorParty += 13;
-                continue;
-            }
-            if (3801 < mmr && mmr < 4100) {
-                valorSolo += 9;
-                valorParty += 16;
-                continue;
-            }
-            if (4101 < mmr && mmr < 4500) {
-                valorSolo += 10.8;
-                valorParty += 19;
-                continue;
-            }
-            if (4501 < mmr && mmr < 5000) {
-                valorSolo += 13.5;
-                valorParty += 21;
-                continue;
-            }
-            if (5001 < mmr && mmr < 5400) {
-                valorSolo += 19;
-                valorParty += 22;
-                continue;
-            }
-            if (5401 < mmr && mmr < 6000) {
-                valorSolo += 32;
-                valorParty += 52;
-                continue;
-            }
-        }
-        // console.log(valorSolo);
-        // console.log(valorParty);
-
-        valorSolo = Math.ceil(valorSolo);
-        valorParty = Math.ceil(valorParty);
-
-        interaction.reply({
-            content: `O valor do boost jogando em sua conta é R$ ${valorSolo} e o valor jogando em party é ${valorParty}`,
-            ephemeral: true,
-        });
-
     }
 }
